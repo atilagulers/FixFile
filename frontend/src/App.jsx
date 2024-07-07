fbfa42b165fbbacad1766fb71c4e1f6bb1207bc8
Atila Guler <atilagulerdev@gmail.com>
2024-07-07 14:54:55 +0300

    implement react-dropzone

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,36 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useCallback} from 'react';
 //import logo from './assets/images/logo-universal.png';
 import './App.css';
-
-import {Greet, GetPath, OrganizeDir} from '../wailsjs/go/main/App';
+import {GetPath, OrganizeDir} from '../wailsjs/go/main/App';
+import {useDropzone} from 'react-dropzone';
 
 function App() {
   const [targetDir, setTargetDir] = useState('');
   const [outputDir, setOutputDir] = useState('');
   //  const [isCopySelected, setIsCopySelected] = useState(true);
 
+  const onDropTarget = useCallback((acceptedFiles) => {
+    console.log('Dropped files in target:', acceptedFiles);
+  }, []);
+
+  const onDropOutput = useCallback((acceptedFiles) => {
+    console.log('Dropped files in output:', acceptedFiles);
+  }, []);
+
+  const {getRootProps: getTargetRootProps, isDragActive: isTargetDragActive} =
+    useDropzone({
+      onDrop: onDropTarget,
+    });
+
+  const {getRootProps: getOutputRootProps, isDragActive: isOutputDragActive} =
+    useDropzone({
+      onDrop: onDropOutput,
+    });
+
   const handleFolderSelection = async (pathFunc) => {
     try {
       const folder = await GetPath();
       pathFunc(folder);
-      console.log(folder);
     } catch (err) {
       console.error(err);
     }
@@ -30,35 +47,60 @@ function App() {
   return (
     <div id="app">
       <h1>Fix File</h1>
+
+      {/*<div style={{backgroundColor: 'gray'}}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>*/}
+
       <div
         style={{
           display: 'flex',
           gap: '32px',
-          padding: '32px',
           width: '100%',
+          marginBottom: '20px',
         }}
       >
-        <div className="folder-input-container">
-          <label className="select-folder-label" htmlFor="target-folder">
-            Target folder:{' '}
-          </label>
-          <button onClick={() => handleFolderSelection(setTargetDir)}>
-            Select Folder
-          </button>
+        <div
+          className="folder-input-container"
+          {...getTargetRootProps()}
+          onClick={() => {
+            handleFolderSelection(setTargetDir);
+          }}
+        >
+          <span>Drag and drop or click to select target folder</span>
           <span>{targetDir}</span>
         </div>
 
-        <div className="folder-input-container">
-          <label className="select-folder-label" htmlFor="output-folder">
-            Output location:
-          </label>
-
-          <button onClick={() => handleFolderSelection(setOutputDir)}>
-            Select Folder
-          </button>
-          <span style={{color: 'white', marginLeft: '10px'}}>{outputDir}</span>
+        <div className="folder-input-container" {...getOutputRootProps()}>
+          <span>Drag and drop or click to select output folder</span>
+          <span>{outputDir}</span>
         </div>
-        {/*<div style={{display: 'flex', justifyContent: 'center', gap: '32px'}}>
+      </div>
+
+      <button
+        onClick={organizeFolder}
+        className="btn-primary"
+        style={{
+          fontSize: '16px',
+          marginTop: '20px',
+          width: '100%',
+        }}
+      >
+        Organize
+      </button>
+    </div>
+  );
+}
+
+export default App;
+
+{
+  /*<div style={{display: 'flex', justifyContent: 'center', gap: '32px'}}>
           <div>
             {' '}
             <label htmlFor="copy">Copy files: </label>
@@ -79,21 +121,5 @@ function App() {
               checked={!isCopySelected}
             />
           </div>
-        </div>*/}
-      </div>
-
-      <button
-        onClick={organizeFolder}
-        className="btn-primary"
-        style={{
-          fontSize: '16px',
-          marginTop: '20px',
-        }}
-      >
-        Organize
-      </button>
-    </div>
-  );
+        </div>*/
 }
-
-export default App;
